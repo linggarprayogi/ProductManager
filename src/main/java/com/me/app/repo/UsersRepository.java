5b0bfccdc8825d7d980510a91d3fe49b125cd9b2
linggarprayogi <linggar.prayogi@gmail.com>
2021-09-12 22:54:02 +0700

    "modify : menambahkan anot @Repository pada UserReporitory.java

--- src/main/java/com/me/app/repo/UsersRepository.java
+++ src/main/java/com/me/app/repo/UsersRepository.java
@@ -1,9 +1,11 @@
 package com.me.app.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.me.app.model.Users;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
