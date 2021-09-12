1e4beb63ae242caf853f686124c621b0b09cfcae
linggarprayogi <linggar.prayogi@gmail.com>
2021-09-12 22:44:29 +0700

    modify: menambahkan annotation @Repository  pada productRepository.java

--- src/main/java/com/me/app/repo/ProductRepository.java
+++ src/main/java/com/me/app/repo/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.me.app.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.me.app.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
