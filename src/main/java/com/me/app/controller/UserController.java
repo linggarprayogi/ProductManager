c535dd1f3905d3af4284be5211909c58195deecf
linggarprayogi <linggar.prayogi@gmail.com>
2022-02-21 17:21:12 +0700

    add login user

--- src/main/java/com/me/app/controller/UserController.java
+++ src/main/java/com/me/app/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.me.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.me.app.model.Users;
 import com.me.app.repo.UsersRepository;
+import com.me.app.services.UserServices;
 
 @Controller
 //@RestController
@@ -17,6 +20,9 @@ public class UserController {
 	@Autowired
 	UsersRepository usersRepository;
 
+	@Autowired
+	UserServices    userServices;
+
 	// Login form
 	@RequestMapping("/")
 	public String login() {
@@ -24,16 +30,35 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/signup/save", method = RequestMethod.POST)
-	public String saveProduct(@ModelAttribute("user") Users user) {
+	public String saveUser(@ModelAttribute("user") Users user) {
 		usersRepository.save(user);
 		return "redirect:/";
 	}
 
 	@RequestMapping("/signup/new")
-	public String showNewproductForm(Model model) {
+	public String showSignUp(Model model) {
 		Users users = new Users();
 		model.addAttribute("users", users);
 
 		return "signup";
+	}
+
+	@RequestMapping("/user-validasi")
+	public String validasiUser(@ModelAttribute("user") Users user) {
+		List<Users> users = userServices.listAll();
+		String      rtn   = null;
+		for (Users u : users) {
+			String usr = u.getUsername();
+			String pwd = u.getPassword();
+
+			if (usr.equals(user.getUsername()) && pwd.equals(user.getPassword())) {
+				rtn = "redirect:/index";
+				break;
+			} else {
+				rtn = "login.html";
+			}
+		}
+
+		return rtn;
 	}
 }
--- src/main/java/com/me/app/services/UserServices.java
+++ src/main/java/com/me/app/services/UserServices.java
@@ -1,8 +1,11 @@
 package com.me.app.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.me.app.model.Users;
 import com.me.app.repo.UsersRepository;
 
 @Service
@@ -11,4 +14,7 @@ public class UserServices {
 	@Autowired
 	UsersRepository usersRepository;
 
+	public List<Users> listAll() {
+		return usersRepository.findAll();
+	}
 }
